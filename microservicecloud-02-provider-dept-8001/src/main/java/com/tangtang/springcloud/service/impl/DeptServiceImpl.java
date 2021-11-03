a999e6cd91949df76d12fe5fbd1905d1b7037cc8
011708380 <011708380@hdjt.hdad.local>
2021-11-03 16:59:14 +0800

    2021-11-03：部门服务提供者

---
+++ microservicecloud-02-provider-dept-8001/src/main/java/com/tangtang/springcloud/service/impl/DeptServiceImpl.java
@@ -0,0 +1,32 @@
+package com.tangtang.springcloud.service.impl;
+
+import com.tangtang.springcloud.dao.DeptDao;
+import com.tangtang.springcloud.entities.Dept;
+import com.tangtang.springcloud.service.DeptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DeptServiceImpl implements DeptService {
+    @Autowired
+    private DeptDao dao;
+
+    @Override
+    public boolean add(Dept dept) {
+        return dao.addDept(dept);
+    }
+
+    @Override
+    public Dept get(Long id) {
+        return dao.findById(id);
+    }
+
+    @Override
+    public List<Dept> list() {
+        return dao.findAll();
+    }
+
+}
+
