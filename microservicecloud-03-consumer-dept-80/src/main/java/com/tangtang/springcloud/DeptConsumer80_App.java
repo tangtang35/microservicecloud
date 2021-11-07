6310b369f684159fdce8a1613ef70058c5c8a9f7
011708380 <011708380@hdjt.hdad.local>
2021-11-07 12:02:25 +0800

    2021-11-07：自定义Ribbo的负载均衡策略:每个机器被访问5次

--- microservicecloud-03-consumer-dept-80/src/main/java/com/tangtang/springcloud/DeptConsumer80_App.java
+++ microservicecloud-03-consumer-dept-80/src/main/java/com/tangtang/springcloud/DeptConsumer80_App.java
@@ -1,15 +1,16 @@
 package com.tangtang.springcloud;
 
+import com.tangtang.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class DeptConsumer80_App
-{
-    public static void main(String[] args)
-    {
+@RibbonClient(name = "MICROSERVICECLOUD-DEPT", configuration = MySelfRule.class)
+public class DeptConsumer80_App {
+    public static void main(String[] args) {
         SpringApplication.run(DeptConsumer80_App.class, args);
     }
 }
--- microservicecloud-03-consumer-dept-80/src/main/java/com/tangtang/springcloud/cfgbeans/ConfigBean.java
+++ microservicecloud-03-consumer-dept-80/src/main/java/com/tangtang/springcloud/cfgbeans/ConfigBean.java
@@ -1,17 +1,29 @@
 package com.tangtang.springcloud.cfgbeans;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import com.netflix.loadbalancer.RetryRule;
+import com.netflix.loadbalancer.RoundRobinRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class ConfigBean
-{
+public class ConfigBean {
     @Bean
     @LoadBalanced   //对ConfigBean进行新注解@LoadBalanced    获得Rest时加入Ribbon的配置
-    public RestTemplate getRestTemplate()
-    {
+    public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
+
+    /**
+     * 默认是轮询算法,可以在这设置其他内置算法
+     * @return
+     */
+    /*@Bean
+    public IRule myIRule(){
+        return new RandomRule();    //用随机算法替换轮询算法，如果将其中一个生产者下线，依旧可以随机
+        //return new RetryRule();    //先按照RoundRobinRule轮询的策略获取服务，如果获取服务失败则在指定时间内会进行重试，获取可用的服务
+    }*/
 }
