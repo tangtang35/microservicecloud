749d1c65507e79cf12b9f0bf3299956754d844a2
011708380 <011708380@hdjt.hdad.local>
2021-11-08 22:14:53 +0800

    2021-11-08：服务熔断,一旦调用服务方法失败并抛出了错误信息后，会自动调用@HystrixCommand标注好的fallbackMethod调用类中的指定方法 ①3个eureka先启动 ②主启动类DeptProvider8001_Hystrix_App ③Consumer启动microservicecloud-consumer-dept-80 ④http://localhost/consumer/dept/get/112

---
+++ microservicecloud-10-provider-dept-hystrix-8001/src/main/java/com/tangtang/springcloud/service/DeptService.java
@@ -0,0 +1,13 @@
+package com.tangtang.springcloud.service;
+
+import com.tangtang.springcloud.entities.Dept;
+
+import java.util.List;
+
+public interface DeptService {
+    public boolean add(Dept dept);
+
+    public Dept get(Long id);
+
+    public List<Dept> list();
+}
